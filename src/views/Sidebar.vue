5ddf097585f65edf1d96daa9d6d638e83965ec16
lepri4dw <lepri4dw@gmail.com>
2024-11-21 23:26:45 +0600

    Added sidebar

--- src/views/Sidebar.vue
+++ src/views/Sidebar.vue
@@ -1,17 +1,28 @@
 <template>
-  <div class="sidebar">
-    <ul>
-      <li><a href="#">Главная</a></li>
-      <li><a href="#">Мой аккаунт</a></li>
-      <li><a href="#">История бронирований</a></li>
-    </ul>
+  <div>
+    <button @click="toggleSidebar" class="toggle-button">Click me</button>
+    <div :class="['sidebar', { open: isOpen }]">
+      <ul>
+        <li><a href="#">Главная</a></li>
+        <li><a href="#">Мой аккаунт</a></li>
+        <li><a href="#">История бронирований</a></li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script setup>
+import { ref } from "vue";
+
+const isOpen = ref(false);
+
+const toggleSidebar = () => {
+  isOpen.value = !isOpen.value;
+};
 </script>
 
 <style scoped>
+
 .sidebar {
   width: 250px;
   background-color: #333;
@@ -20,7 +31,7 @@
   top: 0;
   left: -250px;
   height: 100%;
-  transition: 0.3s;
+  transition: left 0.3s;
 }
 
 .sidebar.open {
@@ -30,6 +41,7 @@
 .sidebar ul {
   list-style-type: none;
   padding: 0;
+  margin: 0;
 }
 
 .sidebar ul li {
@@ -39,5 +51,30 @@
 .sidebar ul li a {
   color: white;
   text-decoration: none;
+}
+
+.toggle-button {
+  position: fixed;
+  top: 20px;
+  left: 260px;
+  padding: 10px 20px;
+  background-color: #6200ea;
+  color: white;
+  border: none;
+  cursor: pointer;
+  border-radius: 5px;
+  transition: background-color 0.3s;
+}
+
+.sidebar.open + .toggle-button {
+  left: 260px;
+}
+
+.sidebar:not(.open) + .toggle-button {
+  left: 10px;
+}
+
+.toggle-button:hover {
+  background-color: #3700b3;
 }
 </style>
