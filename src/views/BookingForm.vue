24378695f5b384a2dc191d54af066ba4a71d6912
Raiset Gible <Kadyrbekovaidar19@gmail.com>
2024-11-21 20:45:21 +0300

    мужики в масле

--- src/views/BookingForm.vue
+++ src/views/BookingForm.vue
@@ -1,11 +1,61 @@
-<script setup>
-
-</script>
-
 <template>
+  <div class="booking-form">
+    <h2>Форма бронирования</h2>
+    <form @submit.prevent="bookSpot">
+      <label for="spotId">Выберите время парковки</label>
+      <label>От:</label>
+      <input type="datetime-local" id="spotId" v-model="spotId" placeholder="" required />
+      <label>До:</label>
+      <input type="datetime-local" id="spotId2" v-model="spotId2" placeholder="" required />
 
+      <button type="submit">Забронировать</button>
+    </form>
+  </div>
 </template>
 
-<style scoped>
+<script setup>
+import { ref } from 'vue';
 
+const spotId = ref('');
+const userName = ref('');
+
+const bookSpot = () => {
+  // Логика бронирования (например, отправка данных на сервер)
+  alert(`Место ${spotId.value} забронировано`);
+};
+</script>
+
+<style scoped>
+.booking-form {
+  max-width: 400px;
+  margin: auto;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  justify-content: center;
+}
+
+.booking-form form {
+  display: flex;
+  flex-direction: column;
+}
+
+.booking-form label {
+  margin-bottom: 8px;
+}
+
+.booking-form input {
+  margin-bottom: 12px;
+  padding: 8px;
+  border-radius: 4px;
+  border: 1px solid #ccc;
+}
+
+.booking-form button {
+  padding: 10px;
+  background-color: #4CAF50;
+  color: white;
+  border: none;
+  border-radius: 4px;
+}
 </style>
